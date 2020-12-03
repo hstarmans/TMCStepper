0cd7d7383777d60c6fe7522015e1c239788476b1
Rik Starmans <hstarmans@gmail.com>
2020-12-03 16:51:37 +0000

    almost ported pybind

--- python/src/main.cpp
+++ python/src/main.cpp
@@ -5,16 +5,11 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(steppers, m) {
     py::class_<TMC2130Stepper>(m, "TMC2130")
-        //.def(py::init<uint16_t, int8_t>())                               // CS, link_index * klopt niet je moet argument zetten!
-        .def(py::init<uint16_t, uint16_t, uint16_t, uint16_t, int8_t>()) // CS, MOSI, MISO, SCK, link_index *
-        .def("begin", &TMC2130Stepper::begin); // check *
-        
-        // volgende zijn setters en getters?
-        //.def("toff", py::overload_cast<uint8_t>(&TMC2130Stepper::toff), "Set the toff time") // check *
-        //.def("test_connection", (py::overload_cast<uint8_t>(&TMC2130Stepper::test_connection)) // check
-        //.def("microsteps", py::overload_cast<uint16_t>(&TMC2130Stepper::microsteps)) // check
-        
-        // snap ik niet?
-        //.def("rms_current", py::overload_cast<uint16_t>(&TMC2130Stepper::rms_current), "Set the max RMS current in mili ampere",
-        // py::arg("mA"), py::arg("multiplier") = 0.5, py::arg("RS") = 0.11); // check
+        .def(py::init<uint16_t, float, int8_t>(), py::arg("pinCS"), py::arg("RS")=0.11, py::arg("link_index"))
+        .def(py::init<uint16_t, uint16_t, uint16_t, uint16_t, int8_t>(), py::arg("pinCS"), py::arg("pinMOSI"), py::arg("pinMISO"), py::arg("pinSCK"), py::arg("link_index"))
+        .def("begin", &TMC2130Stepper::begin)
+        .def("test_connection", &TMC2130Stepper::test_connection)
+        .def("toff", py::overload_cast<uint8_t>(&TMC2130Stepper::toff), "Set the toff time")
+        .def("microsteps", py::overload_cast<uint16_t>(&TMC2130Stepper::microsteps));
+        //.def("rms_current", &TMC2130Stepper::rms_current, "Set the max RMS current in mili ampere", py::arg("mA"), py::arg("mult"));
 }
